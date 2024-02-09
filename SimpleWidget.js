b78967b2f853c6f9b91c788523447661ccf3f89d
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2024-02-09 09:21:04 -0300

    Create SimpleWidget.js

---
+++ SimpleWidget.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+const SimpleWidget = () => {
+
+return (
+<View>
+<Text>
+teste
+</Text>
+
+</View>
+
+);
+
+
+};
+
+export default SimpleWidget;
